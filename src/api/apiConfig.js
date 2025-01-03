9061c9eaa36a9de57274f9cabd868b1d962b4c68
Manzoor Chopan <mchopan21@gmail.com>
2025-01-03 12:32:19 +0530

    new commit

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,4 +1,4 @@
-// const API_BASE_URL = 'https://quickloadbe.onrender.com/' //Office network 
-const API_BASE_URL = 'http://192.168.1.14:4000/' //Home network
+const API_BASE_URL = 'https://quickloadbe.onrender.com/'
+// const API_BASE_URL = 'http://192.168.1.14:4000/' //local network
 
 export default API_BASE_URL;
